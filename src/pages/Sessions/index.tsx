21146652d58a7e98eb9cd5f8a97d8cae55905c06
STAPE <ggggfetfdfgjjh2@gmail.com>
2024-07-23 17:51:16 +0500

    update

--- src/pages/Sessions/index.tsx
+++ src/pages/Sessions/index.tsx
@@ -6,58 +6,59 @@ import { useCallback, useEffect, useState } from "react"
 import { PshycologistsAPI, SessionAPI } from "@/api"
 import { IPsychSession, IPsychSessions, ISession } from "@/types"
 import useGlobalStore from "@/store"
+import { parse, formatISO } from 'date-fns';
 import PsychSidebar from "@/components/PsyhSidebar"
 
 const Session = () => {
-    const navigate = useNavigate()
-    const sid = JSON.parse(localStorage.getItem("sid") as string)
-    const [data, setData] = useState<ISession | null>(null)
-    const [psychData, setPsychData] = useState<IPsychSession | null>(null)
-    const { id } = useParams()
-    const changeCallID = useGlobalStore(state => state.changeCallId)
-    const changePsychId = useGlobalStore(state => state.changePsychId)
-    const changeOpponentName = useGlobalStore(state => state.changeOpponentName)
-    const changeSessionJoined = useGlobalStore(state => state.changeSessionJoined)
-    const userType = localStorage.getItem("userType" as string)
-    const callJoined = useGlobalStore(state => state.callJoined)
-    const [isAvalible, setIsAvalible] = useState(false)
+    const navigate = useNavigate();
+    const sid = JSON.parse(localStorage.getItem("sid") as string);
+    const [data, setData] = useState<ISession | null>(null);
+    const [psychData, setPsychData] = useState<IPsychSession | null>(null);
+    const { id } = useParams();
+    const changeCallID = useGlobalStore(state => state.changeCallId);
+    const changePsychId = useGlobalStore(state => state.changePsychId);
+    const changeOpponentName = useGlobalStore(state => state.changeOpponentName);
+    const changeSessionJoined = useGlobalStore(state => state.changeSessionJoined);
+    const userType = localStorage.getItem("userType" as string);
+    const callJoined = useGlobalStore(state => state.callJoined);
+    const [isAvalible, setIsAvalible] = useState(false);
 
     const getSession = useCallback(async () => {
         if (userType === "psych") {
-            const result: IPsychSessions = await PshycologistsAPI.getMy(sid)
-            const avalible = result.sessions.some(i => i.sesId === id)
-            setPsychData(avalible ? result.sessions.filter(i => i.sesId === id)[0] : null)
-            setIsAvalible(avalible)
+            const result: IPsychSessions = await PshycologistsAPI.getMy(sid);
+            const avalible = result.sessions.some(i => i.sesId === id);
+            setPsychData(avalible ? result.sessions.filter(i => i.sesId === id)[0] : null);
+            setIsAvalible(avalible);
         } else {
-            const result = await SessionAPI.get(sid)
-            const allSessions: ISession[] = result.sessions.future
-            const avalible = allSessions.some(i => i.id === id)
-            setData(avalible ? allSessions.filter(i => i.id === id)[0] : null)
-            setIsAvalible(avalible)
+            const result = await SessionAPI.get(sid);
+            const allSessions: ISession[] = result.sessions.future;
+            const avalible = allSessions.some(i => i.id === id);
+            setData(avalible ? allSessions.filter(i => i.id === id)[0] : null);
+            setIsAvalible(avalible);
         }
-    }, [sid])
+    }, [sid]);
 
     useEffect(() => {
-        getSession()
-    }, [getSession])
+        getSession();
+    }, [getSession]);
 
     useEffect(() => {
         if (data) {
-            changePsychId(data?.psychId)
-            changeOpponentName(data?.psychName)
+            changePsychId(data?.psychId);
+            changeOpponentName(data?.psychName);
         }
 
         if (psychData) {
-            changePsychId(psychData?.userId)
-            changeOpponentName(psychData?.userName)
+            changePsychId(psychData?.userId);
+            changeOpponentName(psychData?.userName);
         }
-    }, [data, psychData])
+    }, [data, psychData]);
 
     useEffect(() => {
         if (id) {
-            changeCallID(id)
+            changeCallID(id);
         }
-    }, [id])
+    }, [id]);
 
     function formatDate(value: string) {
         const months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
@@ -79,53 +80,49 @@ const Session = () => {
     }
 
     async function cancelSession() {
-        const result = await SessionAPI.cancel({ sid, sesId: id })
-        console.log(result)
+        const result = await SessionAPI.cancel({ sid, sesId: id });
+        console.log(result);
         if (result.status === 200) {
-            navigate("/sessions")
+            navigate("/sessions");
         } else {
-            alert(result.msg)
+            alert(result.msg);
         }
     }
 
-    const formatDateToISO = (dateString: string) => {
-        // Разделяем дату и время
-        const [datePart, timePart] = dateString.split(' ');
-        // Форматируем дату и время в ISO 8601
-        return `${datePart}T${timePart.slice(0, -3)}:00.000Z`;
+    const parseDateString = (dateString: string) => {
+        return parse(dateString, 'yyyy-MM-dd HH:mm:ss.SSS', new Date());
     };
 
     const endDate: any = userType === "psych"
-        ? new Date(psychData !== null ? formatDateToISO(psychData?.dateSession) : "2024-04-23T01:10:00.000Z")
-        : new Date(data !== null ? formatDateToISO(data?.dateSession) : "2024-04-23T01:10:00.000Z");
+        ? parseDateString(psychData !== null ? psychData?.dateSession : "2024-04-23 01:10:0.00")
+        : parseDateString(data !== null ? data?.dateSession : "2024-04-23 01:10:0.00");
 
     const difference = endDate - Date.now();
-
 
     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
     const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-    const leftTime = useGlobalStore(state => state.leftTime)
-    const changeLeftTime = useGlobalStore(state => state.changeLeftTime)
+    const leftTime = useGlobalStore(state => state.leftTime);
+    const changeLeftTime = useGlobalStore(state => state.changeLeftTime);
     const calculateTime = useCallback(() => {
-        const time = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+        const time = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
         if (days === 0) {
             if (difference < 0) {
-                changeLeftTime("00:00:00")
+                changeLeftTime("00:00:00");
             } else {
-                changeLeftTime(time)
-                changeSessionJoined(true)
+                changeLeftTime(time);
+                changeSessionJoined(true);
             }
         } else {
             if (difference < 0) {
-                changeLeftTime("00:00:00")
+                changeLeftTime("00:00:00");
             } else {
-                changeLeftTime("")
+                changeLeftTime("");
             }
         }
-    }, [days, hours, minutes, seconds, difference])
+    }, [days, hours, minutes, seconds, difference]);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -139,17 +136,16 @@ const Session = () => {
 
     useEffect(() => {
         if (leftTime === "00:00:00") {
-            changeSessionJoined(isAvalible)
+            changeSessionJoined(isAvalible);
         }
-    }, [leftTime])
+    }, [leftTime]);
 
     useEffect(() => {
         if (window.innerWidth <= 1160 && callJoined) {
-            const elem: HTMLDivElement | null = document.querySelector(".kdokPV9jgsyFtd2e8Ek2")
-            elem?.click()
+            const elem: HTMLDivElement | null = document.querySelector(".kdokPV9jgsyFtd2e8Ek2");
+            elem?.click();
         }
-    }, [callJoined])
-
+    }, [callJoined]);
     return (
         <main className={styles.Page}>
             <section className={styles.Container}>
