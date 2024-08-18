b27907a20907eba3933facc705956537bab73d76
STAPE <ggggfetfdfgjjh2@gmail.com>
2024-08-18 12:22:10 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
-import { BrowserRouter } from "react-router-dom"
-import AppRouter from "@/components/AppRouter"
-import Header from "@/components/Header"
-import VideoCall from "./components/VideoCall"
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "@/components/AppRouter";
+import Header from "@/components/Header";
+import VideoCall from "./components/VideoCall";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const handleScroll = () => {
+      document.documentElement.style.setProperty("--scroll-y", `${window.scrollY}px`);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Header />
-      <VideoCall />
-      <AppRouter />
-    </BrowserRouter>
-  )
+    <div className="App">
+      <div className="Content">
+        <BrowserRouter>
+          <Header />
+          <VideoCall />
+          <AppRouter />
+        </BrowserRouter>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
